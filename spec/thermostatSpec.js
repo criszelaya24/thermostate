7f1a3d7b25cb5d927dd1db42f7b414dd34c4779c
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-27 16:30:39 +0000

    Increasing and decreasing temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,4 +8,13 @@ describe('Thermostat', function(){
 	it('Starts at 20 degree', function(){
 		expect(thermostat.getCurrentTemperature()).toEqual(20);
 	});
+	it('increases in temperature with up()',function(){
+		thermostat.up();
+		expect(thermostat.getCurrentTemperature()).toEqual(21);
+	});
+
+	it('decreases in temperature with up()',function(){
+		thermostat.down();
+		expect(thermostat.getCurrentTemperature()).toEqual(19);
+	});
 });
