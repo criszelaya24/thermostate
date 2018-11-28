1d1201852bed08919b881466a00395b107dfdf9c
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-28 15:50:43 +0000

    Displaying usage level

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -67,4 +67,29 @@ describe('Thermostat', function(){
 			expect(thermostat.getCurrentTemperature()).toEqual(32);
 		});
 	});
+
+	describe('Displaying usage level', function(){
+		describe('When temperature is below 18 degrees',function(){
+			it('it is considered low-usage', function(){
+				for (var i = 0; i < 3; i++) {
+					thermostat.down();
+				}
+				expect(thermostat.energyUsage()).toEqual('low-usage');
+			});
+		});
+		describe('when the temperature is between 18 and 25 degrees', function(){
+			it('it is considered medium-usage', function(){
+				expect(thermostat.energyUsage()).toEqual('medium-usage');
+			});
+		});
+
+		describe('when the temperature is anything else', function(){
+			it('it is considered high-usage', function(){
+				for (var i = 0; i < 6; i++) {
+					thermostat.up();
+				}
+				expect(thermostat.energyUsage()).toEqual('high-usage');
+			});
+		});
+	});
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -6,6 +6,7 @@ function Thermostat() {
 	this.MINIMUM_TEMPERATURE = 10;
 	this.MAX_LIMIT_PSM_ON = 25;
   	this.MAX_LIMIT_PSM_OFF = 32;
+  	this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
 }
 
 Thermostat.prototype.getCurrentTemperature = function() {
@@ -51,4 +52,14 @@ Thermostat.prototype.switchPowerSavingModeOff = function() {
 };
 Thermostat.prototype.switchPowerSavingModeOn = function() {
 	return this.powerSavingMode = true;
+};
+
+Thermostat.prototype.energyUsage = function() {
+	if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+		return 'low-usage';
+	}
+	if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature <= this.MAX_LIMIT_PSM_ON) {
+		return 'medium-usage';
+	}
+	return 'high-usage';
 };
