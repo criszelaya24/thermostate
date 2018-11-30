ba5c622f732640ff23f7db92c8aac6d673d62ef1
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-30 10:59:54 +0000

    Showing the current temperature from others countries

--- src/interface.js
+++ src/interface.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 	var thermostat = new Thermostat();
 	updateTemperature();
+	displayWeather('London');
+
+	console.log($);
 
 	$('#temperature-up').click(function() {
 		thermostat.up();
@@ -32,4 +35,19 @@ $(document).ready(function(){
 		$('#temperature').text(thermostat.getCurrentTemperature() + '°');
 		$('#temperature').attr('class', thermostat.energyUsage());
 	};
+
+	function displayWeather(city) {
+		var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+ 		var token = '&appid=e39f07abdc663f3927948965d78f5d70';
+ 		var units = '&units=metric';
+ 		$.get(url + token + units, function(data) {
+   		  $('#current-temperature').text(data.main.temp + '°' );
+   	    })
+   	};
+
+	$('#current-city').change(function(event) {
+		event.preventDefault();
+		var city = $('#current-city').val();
+		displayWeather(city);
+	});
 });
